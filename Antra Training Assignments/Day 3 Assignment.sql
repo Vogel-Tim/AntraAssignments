076916e70ea0089d6d7e5560bc4728dead615f7c
Tim Vogel <timvogel33@gmail.com>
2022-02-04 10:38:08 -0500

    Day 3 Assignment Partial Completion

--- Antra Training Assignments/Day 3 Assignment.sql
+++ Antra Training Assignments/Day 3 Assignment.sql
@@ -48,9 +48,7 @@ FROM Customers c JOIN Orders o ON c.CustomerID = o.CustomerID
 WHERE c.City != o.ShipCity
 
 --8.      List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
-SELECT TOP 5 SUM(od.Quantity) AS UnitsOrdered, od.ProductID, AVG(od.unitPrice) AveragePrice, (SELECT c.City FROM customers c JOIN Orders o ON c.CustomerID = o.CustomerID JOIN [Order Details] od ON o.OrderID = od.OrderID GROUP BY c.City ORDER BY SUM(od.Quantity) DESC)
-FROM [Order Details] od
-GROUP BY od.ProductID
+
 --9.      List all cities that have never ordered something but we have employees there.
 
 --a.      Use sub-query
@@ -60,3 +58,10 @@ GROUP BY od.ProductID
 --10.  List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
 
 --11. How do you remove the duplicates record of a table?
+--WITH CTE AS
+--(
+--SELECT *,ROW_NUMBER() OVER (PARTITION BY col1,col2,col3 ORDER BY col1,col2,col3) AS RN
+--FROM MyDuplicateTable
+--)
+
+--DELETE FROM CTE WHERE RN<>1
